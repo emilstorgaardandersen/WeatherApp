8eb33b366fa582e901cfac000bacb6f17995162c
Emil <emil@storgaardandersen.dk>
2020-06-26 10:23:06 +0200

    Added homepage

--- weatherApp/pages/HomeScreen.js
+++ weatherApp/pages/HomeScreen.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default class SecondPage extends React.Component {
     render() {
         return (
             <View style={styles.screen}>
+                <View style={styles.image}>
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/01d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/02d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/03d@2x.png' }} />
+                </View>
+                <View style={styles.image}>
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/04d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/09d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/10d@2x.png' }} />
+                </View>
+
                 <View style={styles.button}>
                     <Text style={styles.text} onPress={() => this.props.navigation.navigate('Weather')}>Welcome to Weather APP</Text>
+                </View>
+
+                <View style={styles.image}>
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/11d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/13d@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/01n@2x.png' }} />
+                </View>
+                <View style={styles.image}>
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/02n@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/10n@2x.png' }} />
+                    <Image style={styles.iconSize} source={{ uri: 'http://openweathermap.org/img/wn/50n@2x.png' }} />
                 </View>
             </View>
         )
@@ -26,12 +48,21 @@ const styles = StyleSheet.create({
         backgroundColor: 'gray',
         justifyContent: 'center',
         alignItems: 'center',
-        margin: 10,
 
     },
     text: {
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: 'bold',
-        padding: 10,
-    }
+        padding: 20,
+    },
+    image: {
+        flex: 1,
+        flexDirection: 'row'
+
+    },
+    iconSize: {
+        width: 100,
+        height: 100,
+        resizeMode: 'stretch',
+    },
 });
